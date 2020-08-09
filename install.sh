695e6c1b2fa5f78d05edf10e67a7f210bf623318
Endang Suwarna <edsuwarna@192.168.0.108>
2020-08-10 05:20:28 +0700

    add shell script for install filebrowser

--- install.sh
+++ install.sh
@@ -10,4 +10,11 @@ echo "Installing Nginx"
 sudo apt install nginx
 sudo /etc/init.d/nginx start
 
+# Installing Filebrowser
 
+echo "Installing Filebrowser"
+curl -fsSL https://filebrowser.org/get.sh | bash
+cd /usr/local/bin
+
+echo "running Filebrowser"
+sudo filebrowser 
